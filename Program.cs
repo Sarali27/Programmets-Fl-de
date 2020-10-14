eac7b27d437a1b8df45f26bd18c8f5729301b902
Sarah Ali <64776746+Sarali27@users.noreply.github.com>
2020-10-14 13:03:17 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Countdown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+
+            static void Start()
+            {
+                Console.Read();
+        
+            }
+        {
+            Console.WriteLine("Vänta lite");
+        }
+        
+        }
+    
+
+
+
+    }
+}
