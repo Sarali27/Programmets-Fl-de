405cda03ef21fdd2b18e4eeb555ac453ad2ce587
Andreas Gunnarsson <andreas.gunnarsson@gmail.com>
2020-10-14 13:30:29 +0200

    Added StartStop class.

---
+++ StartStop.cs
@@ -0,0 +1,36 @@
+﻿using System;
+// TODO: Vilka usings krävs för Random och TimeSpan? "using System" fungerar som någon slags "allt-i-allo".
+using System.Diagnostics;       // Stopwatch
+using System.Threading;
+
+namespace lessson6_pause_program
+{
+    class Countdown
+    {
+        private Random random = new Random();
+        private Stopwatch stopwatch = new Stopwatch();
+        private TimeSpan timeElapsed;
+
+        public void RandomSleep()
+        {
+            int randomValue = random.Next(500, 5001);
+            Thread.Sleep(randomValue);
+        }
+
+        public void StartTimer()
+        {
+            stopwatch.Start();
+        }
+
+        public TimeSpan StopTimer()
+        {
+            stopwatch.Stop();
+            return timeElapsed = stopwatch.Elapsed;
+        }
+
+        public void ResetTimer()
+        {
+            stopwatch.Reset();
+        }
+    }
+}
